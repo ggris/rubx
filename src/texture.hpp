cf9402a025f670f5afb016db925bfdc8788683f5
Chantal Ding <cding@c128-12.enst.fr>
2015-04-16 14:34:27 +0200

    creating Texture class

---
+++ src/texture.hpp
@@ -0,0 +1,16 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "util/include_gl.hpp"
+
+class Texture
+{
+    public:
+        Texture(const std::string &filename);
+        GLuint getTexture() const {return texture_}
+    private:
+        const std::string filename_;
+        const GLuint texture_;
+};
+
+#endif // TEXTURE_H
